35cb822cc48cfca3b2090fe6443d550da7e67366
NaufalYogaPratama <naufaltino2113@gmail.com>
2025-09-22 22:29:25 +0700

    feat(add): add a logo and footer

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -26,6 +26,7 @@ import Navbar from "../components/Navbar";
 import QrScanner from "../components/QrScanner";
 import RiwayatObatModal from "../components/RiwayatObatModal";
 import HeroImage from "../assets/hero.png";
+import Logo from "../assets/logo.png";
 
 const LandingPage = () => {
   const [showScanner, setShowScanner] = useState(false);
@@ -528,30 +529,7 @@ const LandingPage = () => {
               {/* Kolom Brand */}
               <div className="col-span-2 md:col-span-1">
                 <div className="flex items-center space-x-2 mb-4">
-                  <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-8 w-8 text-[#16A34A]"
-                  >
-                    <path
-                      d="M12 2L2 7V17L12 22L22 17V7L12 2Z"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinejoin="round"
-                    />
-                    <path
-                      d="M2 7L12 12M22 7L12 12M12 22V12M17 4.5L7 9.5"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                  <span className="text-2xl font-bold text-gray-900">
-                    MediSync
-                  </span>
+                  <img src={Logo} alt="MediSync Logo" className="h-20 w-auto" />
                 </div>
                 <p className="text-gray-500 text-sm">
                   Mengamankan rantai pasok farmasi dengan teknologi blockchain.
